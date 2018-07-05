6966d9e3822763fcec13900985e9478fc9c1c07c
Rekha G Naik <rekha.g.naik@boeing.com>
2018-07-06 00:51:27 +0530

    post action changed

--- api/routes/ifb.js
+++ api/routes/ifb.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+
 var AWS = require("aws-sdk");
 AWS.config.update({
     region: "us-east-1"
@@ -8,100 +11,41 @@ AWS.config.update({
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 var table = "opwbids";
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
 
 
-
-/*router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling GET requests to /products'
     });
-});*/
+});
+
+router.get('/dbTables', (req, res, next) => {
+      console.log("entered into dynadb route - list tables");
+      ddb.listTables({Limit: 10}, function(err, data) {
+            if (err) {
+                console.log("Error", err.code);
+            } else {
+                console.log("Table names are ", data.TableNames);
+                res.status(200).json({
+                 TableNames: data.TableNames
+                    });
+            }
+      });
+});
 
 router.post('/addbid', (req, res, next) => {
-    var bidid = "1";
+    var requestdata = req.body;
     var params = {
         TableName:table,
-        Item:{
-            "bidid": bidid,
-            "Client":{
-                "Name": "The Client Company Inc.",
-                "Sites": ["205, Arbor Oaks Summerville", "200, Arbor Oaks Summerville"]
-            },
-            "BidDueDate": "07/03/2018",
-            "WorkDueDate": "10/03/2018",
-            "Description": "Installs pipes and fixtures, such as sinks and toilets, for water, gas, steam, air, or other liquids Installs supports for pipes, equipment, and fixtures prior to installation Assembles fittings and valves for installation Modifies length of pipes, fixtures, and other plumbing materials as needed for a building Uses saws and pipe cutters as necessary Installs heating and air-conditioning systems, including water heaters Collaborates with contractors, construction workers, electricians, pipefitters, and steamfitters in installing and repairing plumbing Tests plumbing systems for leaks and other problems Analyses problem and identifies appropriate tools and materials for repair Chooses plumbing materials based on budget, location, and intended uses of building Follows health and safety standards and complies with building codes Writes report documenting the problem and summary of actions taken Performs inspections of plumbing systems to identify and replace worn parts Prepares bids and schedules and oversees other workers, such as apprentices and helpers",
-            "Documents":[
-                {
-                    "Name": "ABC.doc",
-                    "URL": "https://ABC.doc"
-                },
-                {
-                    "Name": "XYZ.doc",
-                    "URL": "https://XYZ.doc"
-                }
-            ],
-            "Vendors":[
-                {
-                    "Name": "Vendor 1",
-                    "Type": "Alarms",
-                    "Status": "Open/Closed/Waiting",
-                    "Price": "1000",
-                    "Comments":[
-                        {
-                            "question": "Value?",
-                            "reply": "Ans"
-                        },
-                        {
-                            "question": "Value?",
-                            "reply": "Ans"
-                        }
-                    ],
-
-                    "BidPackage":[
-                        {
-                            "Name": "ABC.doc",
-                            "URL": "https://ABC.doc"
-                        },
-                        {
-                            "Name": "XYZ.doc",
-                            "URL": "https://XYZ.doc"
-                        }
-                    ]
-                },
-                {
-                    "Name": "Vendor 1",
-                    "Type": "Alarms",
-                    "Status": "Open/Closed/Waiting",
-                    "Price": "1000",
-                    "Comments":[
-                        {
-                            "question": "Value?",
-                            "reply": "Ans"
-                        },
-                        {
-                            "question": "Value?",
-                            "reply": "Ans"
-                        }
-                    ],
-
-                    "BidPackage":[
-                        {
-                            "Name": "ABC.doc",
-                            "URL": "https://ABC.doc"
-                        },
-                        {
-                            "Name": "XYZ.doc",
-                            "URL": "https://XYZ.doc"
-                        }
-                    ]
-                }
-            ]
-        }
-    };
-    console.log("Adding a new item...");
+        Item: requestdata
+    }
+    console.log('Before' + params.Item);
     docClient.put(params, function(err, data) {
+        console.log('inside put action' + err);
         if (err) {
-            res.status(500).json(JSON.stringify(err, null, 2));
+            res.status(500).json(err);
         } else {
             res.status(201).json(JSON.stringify(data, null, 2));
         }
@@ -123,6 +67,7 @@ router.get('/:bidId', (req, res, next) => {
             res.status(404).json(JSON.stringify(err, null, 2));
         } else {
             res.status(200).json(JSON.stringify(data, null, 2));
+            console.log(' data created -- ' + JSON.stringify(data));
         }
     });
 
--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const ifbRoutes = require('./api/routes/ifb');
 
+//app.use(bodyParser);
+//app.use(express.bodyParser());
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+
 app.use(morgan('dev'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Routes which should handle requests
 app.use('/ifb', ifbRoutes);
+
 
 
 app.use((req, res, next) => {
@@ -24,5 +40,6 @@ app.use((error, req, res, next) => {
         }
     });
 });
+
 
 module.exports = app;
